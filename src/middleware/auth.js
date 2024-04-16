744341a28cc08612414d64fbbd118ab0f7a7a816
cristianRG <cristianrizogomez890@gmail.com>
2024-04-16 02:51:33 -0600

    changes in cookie get

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,40 +1,29 @@
 import axios from 'axios'
-
-const API_BASE_URL = 'https://soundscape-server.onrender.com'; // O utiliza config.ORIGIN_URL_SERVER si lo has definido en otro lugar
-
-export async function isAuthenticated(token) {
-    try {
-        if (token) {
-            const result = await axios.post(`${API_BASE_URL}/api/v1/blog/auth`, { token });
-            return result.status === 200;
-        }
-        return false;
-    } catch (error) {
-        console.error('Error en la autenticación:', error);
-        return false;
+export async function isAuthenticated(token){
+    if(token!=null){
+        const result = await axios.post(`https://soundscape-server.onrender.com/api/v1/blog/auth`, {token: token})
+        
+        return result.status == 200
     }
+    return false
 }
 
-export async function isAdmin(token) {
-    try {
-        if (token) {
-            const result = await axios.post(`${API_BASE_URL}/api/v1/blog/dashboard/auth`, { token });
-            return result.status === 200;
-        }
-        return false;
-    } catch (error) {
-        console.error('Error en la verificación de administrador:', error);
-        return false;
+export async function isAdmin(token){
+    if(token!=null){
+        const result = await axios.post(`https://soundscape-server.onrender.com/api/v1/blog/dashboard/auth`, {token: token})
+        
+        return result.status == 200
     }
+    return false
 }
 
-export function getTokenFromCookie(cookieString) {
-    const cookies = cookieString.split(';');
+export function getTokenFromCookie() {
+    const cookies = document.cookie.split(';');
     for (let i = 0; i < cookies.length; i++) {
         const cookie = cookies[i].trim();
         if (cookie.startsWith('token=')) {
             return cookie.substring('token='.length, cookie.length);
         }
     }
-    return null;
+    return null; // Si no se encuentra el token en la cookie
 }
